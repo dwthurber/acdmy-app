22039c23f22f97e8c2d163486d784477ce3ba71a
Derek Thurber <derek.thurber@northwestern.edu>
2017-09-08 17:05:43 -0500

    added sessions db

--- src/components/main/ManageSessions.vue
+++ src/components/main/ManageSessions.vue
@@ -16,13 +16,21 @@
         <div class="modal-card" @click.stop>
           <section class="modal-card-body">
             <p class="subtitle is-3">Schedule New Session</p>
-            <b-field label="Session Title">
-              <b-input icon="view_list" placeholder="e.g. Week 1" v-model="sessionName"></b-input>
+            <hr>
+            <b-field>
+              <b-input icon="view_list" placeholder="Session Name" v-model="sessionName"></b-input>
             </b-field>
-            <b-field label="Session Date">
+            <br>
+            <p class="subtitle has-text-primary is-5">Details</p>
+            <b-field>
+              <p class="control">
+                <a class="button is-static">
+                  Start
+                </a>
+              </p>
               <b-datepicker
                 :min-date="minDate"
-                v-model="date"
+                v-model="startDate"
                 placeholder="Click to select..."
                 icon="today">
 
@@ -38,11 +46,140 @@
                   <span>Clear</span>
                 </button>
               </b-datepicker>
+              <b-select placeholder="Time" v-model="startTime">
+                <option>12:00 am</option>
+                <option>12:30 am</option>
+                <option>1:00 am</option>
+                <option>1:30 am</option>
+                <option>2:00 am</option>
+                <option>2:30 am</option>
+                <option>3:00 am</option>
+                <option>3:30 am</option>
+                <option>4:00 am</option>
+                <option>4:30 am</option>
+                <option>5:00 am</option>
+                <option>5:30 am</option>
+                <option>6:00 am</option>
+                <option>6:30 am</option>
+                <option>7:00 am</option>
+                <option>7:30 am</option>
+                <option>8:00 am</option>
+                <option>8:30 am</option>
+                <option>9:00 am</option>
+                <option>9:30 am</option>
+                <option>10:00 am</option>
+                <option>10:30 am</option>
+                <option>11:00 am</option>
+                <option>11:30 am</option>
+                <option>12:00 pm</option>
+                <option>12:30 pm</option>
+                <option>1:00 pm</option>
+                <option>1:30 pm</option>
+                <option>2:00 pm</option>
+                <option>2:30 pm</option>
+                <option>3:00 pm</option>
+                <option>3:30 pm</option>
+                <option>4:00 pm</option>
+                <option>4:30 pm</option>
+                <option>5:00 pm</option>
+                <option>5:30 pm</option>
+                <option>6:00 pm</option>
+                <option>6:30 pm</option>
+                <option>7:00 pm</option>
+                <option>7:30 pm</option>
+                <option>8:00 pm</option>
+                <option>8:30 pm</option>
+                <option>9:00 pm</option>
+                <option>9:30 pm</option>
+                <option>10:00 pm</option>
+                <option>10:30 pm</option>
+                <option>11:00 pm</option>
+                <option>11:30 pm</option>
+              </b-select>
             </b-field>
-            <b-field label="Session Length">
-              <b-input icon="timelapse" type="number" placeholder="Default: 3 hours" v-model="sessionLength"></b-input>
+            <b-field>
+              <p class="control">
+                <a class="button is-static">
+                  End
+                </a>
+              </p>
+              <b-datepicker
+                :min-date="minDate"
+                v-model="endDate"
+                placeholder="Click to select..."
+                icon="today">
+
+                <button class="button is-primary"
+                  @click="date = new Date()">
+                  <b-icon icon="today"></b-icon>
+                  <span>Today</span>
+                </button>
+
+                <button class="button is-danger"
+                  @click="date = null">
+                  <b-icon icon="clear"></b-icon>
+                  <span>Clear</span>
+                </button>
+              </b-datepicker>
+              <b-select placeholder="Time" v-model="endTime">
+                <option>12:00 am</option>
+                <option>12:30 am</option>
+                <option>1:00 am</option>
+                <option>1:30 am</option>
+                <option>2:00 am</option>
+                <option>2:30 am</option>
+                <option>3:00 am</option>
+                <option>3:30 am</option>
+                <option>4:00 am</option>
+                <option>4:30 am</option>
+                <option>5:00 am</option>
+                <option>5:30 am</option>
+                <option>6:00 am</option>
+                <option>6:30 am</option>
+                <option>7:00 am</option>
+                <option>7:30 am</option>
+                <option>8:00 am</option>
+                <option>8:30 am</option>
+                <option>9:00 am</option>
+                <option>9:30 am</option>
+                <option>10:00 am</option>
+                <option>10:30 am</option>
+                <option>11:00 am</option>
+                <option>11:30 am</option>
+                <option>12:00 pm</option>
+                <option>12:30 pm</option>
+                <option>1:00 pm</option>
+                <option>1:30 pm</option>
+                <option>2:00 pm</option>
+                <option>2:30 pm</option>
+                <option>3:00 pm</option>
+                <option>3:30 pm</option>
+                <option>4:00 pm</option>
+                <option>4:30 pm</option>
+                <option>5:00 pm</option>
+                <option>5:30 pm</option>
+                <option>6:00 pm</option>
+                <option>6:30 pm</option>
+                <option>7:00 pm</option>
+                <option>7:30 pm</option>
+                <option>8:00 pm</option>
+                <option>8:30 pm</option>
+                <option>9:00 pm</option>
+                <option>9:30 pm</option>
+                <option>10:00 pm</option>
+                <option>10:30 pm</option>
+                <option>11:00 pm</option>
+                <option>11:30 pm</option>
+              </b-select>
             </b-field>
-            <b-field label="Starting Layout">
+            <br>
+            <p class="subtitle has-text-primary is-5">Options</p>
+            <b-field>
+              <p class="control">
+                <a class="button is-static">
+                  Starting Layout
+                </a>
+              </p>
               <b-select placeholder="Layout" icon="view_compact" v-model="layout">
                 <option value="1">Video Bar</option>
                 <option value="2">Full Screen</option>
@@ -51,7 +188,7 @@
               </b-select>
             </b-field>
             <hr>
-            <a class="button is-success">Schedule Session</a>
+            <a class="button is-success" @click="scheduleSession">Schedule Session</a>
             <a class="button" @click="isModalActive = false">Cancel</a>
           </section>
         </div>
@@ -72,42 +209,139 @@
     </div>
     <div class="box">
       <p class="subtitle is-uppercase is-7 has-text-grey">All Sessions</p>
+      <b-table
+        :data="sessions"
+        :bordered="isBordered"
+        :striped="isStriped"
+        :narrowed="isNarrowed"
+        :checkable="isCheckable"
+        :loading="isLoading"
+        :mobile-cards="hasMobileCards"
+        :paginated="isPaginated"
+        :per-page="perPage"
+        :pagination-simple="isPaginationSimple"
+        default-sort="startDate"
+        :selected.sync="selected"
+        :isDetailed="isDetailed"
+        :checked-rows.sync="checkedRows">
+
+        <template scope="props">
+          <b-table-column field="name" label="Name" sortable>
+            {{ props.row.name }}
+          </b-table-column>
+          <b-table-column field="startDate" label="Start" sortable>
+            {{ props.row.startDate | formatDate }} at {{ props.row.startTime }}
+          </b-table-column>
+          <b-table-column field="endDate" label="End" sortable>
+            {{ props.row.endDate | formatDate }} at {{ props.row.endTime }}
+          </b-table-column>
+          <b-table-column field="" label="">
+            <button class="button is-danger is-outlined is-small" @click="deleteSession(props.row['.key']); isDeleteRoomActive = true"><b-icon icon="delete_forever" size="is-small"></b-icon></button>
+          </b-table-column>
+        </template>
+
+        <div slot="empty" class="has-text-centered">
+          No Sessions
+        </div>
+      </b-table>
     </div>
   </div>
 </template>
 
 <script>
-import { sessionsRef } from '@/firebase'
+import { db, sessionsRef, roomsRef } from '@/firebase'
 import { mapState } from 'vuex'
+import moment from 'moment'
 
 export default {
   name: 'Dashboard-Sessions',
   computed: {
     ...mapState(['sessions', 'route'])
   },
+  filters: {
+    formatDate: function (value) {
+      if (value) {
+        return moment(String(value)).format('dddd MMM Do YYYY')
+      }
+    }
+  },
   data () {
     return {
       isModalActive: false,
       minDate: null,
       sessionName: null,
-      date: new Date(),
-      sessionLength: null,
-      layout: 1
+      startDate: new Date(),
+      startTime: null,
+      endDate: new Date(),
+      endTime: null,
+      layout: 1,
+      checkedRows: [],
+      selected: {},
+      isBordered: false,
+      isStriped: true,
+      isNarrowed: false,
+      isCheckable: false,
+      isEmpty: false,
+      isLoading: true,
+      isDetailed: false,
+      hasMobileCards: true,
+      isPaginated: false,
+      isPaginationSimple: false,
+      perPage: 10
     }
   },
   created () {
     this.setSessions()
     const today = new Date()
     this.minDate = new Date(today.getFullYear(), today.getMonth(), today.getDate() - 0)
+    this.isLoading = false
   },
   methods: {
     setSessions () {
       this.$store.dispatch('setSessions', sessionsRef.child(this.route.params.roomid))
     },
+    scheduleSession () {
+      let newSessionKey = sessionsRef.push().key
+      const newSession = {
+        name: this.sessionName,
+        startDate: this.startDate,
+        startTime: this.startTime,
+        endDate: this.endDate,
+        endTime: this.endTime,
+        layout: this.layout
+      }
+      // const increment = {
+      //   sessions: 1
+      // }
+      let updates = {}
+      updates['/sessions/' + this.route.params.roomid + '/' + newSessionKey] = newSession
+      updates['/rooms/' + this.route.params.roomid + '/sessions/' + newSessionKey] = true
+      db.ref().update(updates)
+
+      this.isModalActive = false
+      this.$toast.open('Session scheduled')
+    },
     confirm () {
       this.$dialog.confirm({
         message: 'Are you sure you want to start a session with your default settings now?',
         onConfirm: () => this.$toast.open('Session start confirmed')
+      })
+    },
+    deleteSession: function (key) {
+      const toast = this.$toast
+      // const uid = this.user.uid
+      this.$dialog.confirm({
+        title: 'Deleting Session',
+        message: 'Are you sure you want to <strong>delete</strong> this session? This action cannot be undone.',
+        confirmText: 'Delete Session',
+        type: 'is-danger',
+        hasIcon: true,
+        onConfirm: () => {
+          // then remove room and associated nodes
+          sessionsRef.child(this.route.params.roomid).child(key).remove()
+          roomsRef.child(this.route.params.roomid).child('sessions').child(key).remove()
+          toast.open('Session removed')
+        }
       })
     }
   }
