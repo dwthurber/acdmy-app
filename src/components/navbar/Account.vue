36e50c0019d03015e777545bf88aed472d3e8fdd
Derek Thurber <dwthurber@gmail.com>
2017-10-06 16:09:17 -0500

    fixed account settings mobile sizing

--- src/components/navbar/Account.vue
+++ src/components/navbar/Account.vue
@@ -1,5 +1,5 @@
 <template>
-  <b-dropdown position="is-bottom-left">
+  <b-dropdown position="is-bottom-left" :hoverable="isAccountModalActive">
     <a class="navbar-item"  slot="trigger" title="user profile">
       <img v-if="user.photoURL" class="is-circle-image image is-32x32" :src="user.photoURL" alt="Profile Image">
       <img v-else class="is-circle-image image is-32x32" src="../../assets/user-placeholder.png" alt="Profile Image">
@@ -16,7 +16,7 @@
                 <small class="has-text-danger" v-if="!user.emailVerified && !emailsent && !isSending"><a class="has-text-danger"  @click.prevent="verifyEmail">Please verify email</a></small>
                 <small class="has-text-grey" v-if="isSending">Sending email...</small>
               </p>
-              <p><small><a @click="isAccountModalActive = true">Account</a></small> | <small><a  @click.self.prevent="logout">Logout</a></small></p>
+              <p><small><a @click="openAccountSettings()">Account</a></small> | <small><a  @click.self.prevent="logout">Logout</a></small></p>
             </p>
           </div>
         </div>
@@ -32,37 +32,6 @@
     <b-dropdown-item class="dropdown-item">
       <b-icon icon="videocam" /> Test Video Setup
     </b-dropdown-item>
-    <!-- <b-dropdown-option custom>
-      <b-field>
-        <b-select placeholder="Video Source" icon="videocam" expanded>
-          <option value="1">Option 1</option>
-          <option value="2">Option 2</option>
-        </b-select>
-      </b-field>
-      <b-field>
-        <b-select placeholder="Audio Input Source" icon="mic" expanded>
-          <option value="1">Option 1</option>
-          <option value="2">Option 2</option>
-        </b-select>
-      </b-field>
-      <b-field>
-        <b-select placeholder="Audio Output Source" icon="volume_up" expanded>
-          <option value="1">Option 1</option>
-          <option value="2">Option 2</option>
-        </b-select>
-      </b-field>
-    </b-dropdown-option>
-    <hr class="dropdown-divider">
-    <b-dropdown-option custom>
-      <div class="columns">
-        <div class="column">
-          <button class="button is-dark is-outlined is-wide">Room Settings</button>
-        </div>
-        <div class="column">
-          <button class="button is-dark is-outlined is-wide">Student View </button>
-        </div>
-      </div>
-    </b-dropdown-option> -->
     <b-modal :active.sync="isAccountModalActive" has-modal-card canCancel>
       <modal-account></modal-account>
     </b-modal>
@@ -115,6 +84,10 @@ export default {
           console.error('Email Error', error)
           this.isSending = false
         })
+    },
+    openAccountSettings () {
+      this.$emit('active-change', false)
+      this.isAccountModalActive = true
     }
   }
 }
